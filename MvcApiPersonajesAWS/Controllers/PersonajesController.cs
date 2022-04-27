d777e3865cea38b97b40f758d5cfd6c58eece5d8
Serra <pacoserranox@gmail.com>
2022-04-27 13:26:09 +0200

    Add project files.

---
+++ MvcApiPersonajesAWS/Controllers/PersonajesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcApiPersonajesAWS.Controllers
+{
+    public class PersonajesController : Controller
+    {
+        public IActionResult IndexCliente()
+        {
+            return View();
+        }
+    }
+}
