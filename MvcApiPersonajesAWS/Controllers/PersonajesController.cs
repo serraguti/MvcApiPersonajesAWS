c3e46281228c48081e4c644fc341273c8faa4246
serraguti <pacoserranox@gmail.com>
2022-04-28 14:11:53 +0200

    commit

--- MvcApiPersonajesAWS/Controllers/PersonajesController.cs
+++ MvcApiPersonajesAWS/Controllers/PersonajesController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiPersonajesAWS.Models;
+using MvcApiPersonajesAWS.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +10,31 @@ namespace MvcApiPersonajesAWS.Controllers
 {
     public class PersonajesController : Controller
     {
+        private ServiceApiPersonajes service;
+
+        public PersonajesController(ServiceApiPersonajes service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> IndexServidor()
+        {
+            List<Personaje> personajes = await this.service.GetPersonajesAsync();
+            return View(personajes);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Personaje personaje)
+        {
+            await this.service.InsertPersonajeAsync(personaje);
+            return RedirectToAction("IndexServidor");
+        }
+
         public IActionResult IndexCliente()
         {
             return View();
